ad0ddee90bbae24ccad75bf3fb68b15518060d4e
wDKidd <kzj.kidd@gmail.com>
2015-12-20 15:45:25 +0200

    adds search component

---
+++ app/assets/javascripts/components/search.js.jsx
@@ -0,0 +1,18 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// provides functionality for search input, ensuring
+// it is not read-only
+var SearchInput = React.createClass({
+  getInitialState: function() {
+    return {value: ''};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+  },
+  render: function() {
+    var value = this.state.value;
+    return (<input placeholder="Search.." type="text" value={value} onChange={this.handleChange}/>)
+  }
+});
